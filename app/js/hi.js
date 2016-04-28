d1c50dd7dd34793d710e4c757a4d0c46613ae2cf
Luis Pena <ljpenaurena@gmail.com>
2016-04-27 23:28:05 -0400

    Playing around with typescript

--- app/js/hi.js
+++ app/js/hi.js
@@ -1,11 +1,18 @@
-var test;
-(function (test) {
-    var klk = (function () {
-        function klk() {
-        }
-        klk.val = function () {
-            return false;
-        };
-        return klk;
-    }());
-})(test || (test = {}));
+var Greeter = (function () {
+    function Greeter(name) {
+        this.name = name;
+    }
+    Greeter.prototype.greet = function () {
+        alert(this.getName());
+    };
+    Greeter.prototype.getName = function () {
+        return this.name;
+    };
+    return Greeter;
+}());
+window.onload = function () {
+    var gree = new Greeter("Luis");
+    var greeter = gree;
+    console.log(greeter);
+    greeter.greet();
+};
--- app/typescripts/hi.ts
+++ app/typescripts/hi.ts
@@ -1,11 +1,29 @@
-module test {
-    class klk {
 
+interface  Greet{
+    greet():void;
+}
 
-        public static val():Boolean {
-            return false;
-        }
+class Greeter implements Greet{
 
+    private name: string;
+    constructor(name: string)
+    {
+        this.name = name;
     }
 
+    public greet():void{
+        alert(this.getName());
+    }
+
+    public getName():string{
+        return this.name;
+    }
 }
+
+window.onload = function(){
+    let gree = new Greeter("Luis");
+    let greeter = <Greet> gree;
+
+   console.log(greeter);
+    greeter.greet();
+};
